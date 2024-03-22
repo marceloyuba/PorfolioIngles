a15639995c0e964e23899df162b7138d2e59ed46
marceloyuba <marceloyuba@gmail.com>
2024-03-22 14:57:07 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ page_bg_img = f"""
 <style>
 
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://github.com/marceloyuba/PorfolioIngles/blob/main/scr/fondo.jpg");
+background-image: url("https://github.com/marceloyuba/PorfolioIngles/blob/main/scr/fondo.jpg?raw=true");
 background-size: 100%;
 background-position: top left;
 background-repeat: no-repeat;
