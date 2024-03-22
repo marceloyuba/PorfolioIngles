dc05a6cbb265b0e484e491b8d54b24dfc4529822
marceloyuba <marceloyuba@gmail.com>
2024-03-22 15:33:07 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.set_page_config(page_icon="scr/fondo.jpg", layout="wide")
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
 page_bg_img = f"""
 <style>
 
