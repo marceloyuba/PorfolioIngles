1ef0a3811641c0e6b9ade95d2f754a3b9bd74d15
marceloyuba <marceloyuba@gmail.com>
2024-03-22 16:40:24 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,22 @@ font-color
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hola, soy Marcelo Yuba :wave:")
+    st.title("Un Data Analyst  y Data Scientist de Buenos Aires, Argentina")
+    st.write(
+        "Soy un apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para tu negocio."
+    )
+    st.write("[Mi Github >](https://github.com/marceloyuba)")
+    st.write("[Mi LinkedIn >](https://www.linkedin.com/in/marcelo-yuba-b9a39827b/)")
+
+def main():
+    
+    st.title('Dashboard de Power BI en Streamlit')
+    
+    # Inserta el iframe con la URL de tu dashboard de Power BI
+    st.markdown('<iframe width="1000" height="800" align = "center" src="https://app.powerbi.com/view?r=eyJrIjoiM2NjNDA0YmItMmRhZC00ZDhlLWFmOWYtZTZiMWMxYWY3ODAzIiwidCI6ImUyYjc5Nzc5LTBhODgtNDMzMS05YjQyLTM4NGNkNzFjODVkNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+        
+if __name__ == '__main__':
+    main()
