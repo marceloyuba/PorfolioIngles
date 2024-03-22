40e3887d58176b736a8c2336c059b566973d723e
marceloyuba <marceloyuba@gmail.com>
2024-03-22 14:24:42 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,19 @@
 import streamlit as st
 
-st.set_page_config(page_title="Marcelo Yuba's Portfolio", page_icon="scr/fondo.jpg", layout="wide")
+page_bg_img = f"""
+<style>
 
-# ---- HEADER SECTION ----
-with st.container():
-    st.subheader("Hola, soy Marcelo Yuba :wave:")
-    st.title("Un Data Analyst  y Data Scientist de Buenos Aires, Argentina")
-    st.write(
-        "Soy un apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para tu negocio."
-    )
-    st.write("[Mi Github >](https://github.com/marceloyuba)")
-    st.write("[Mi LinkedIn >](https://www.linkedin.com/in/marcelo-yuba-b9a39827b/)")
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-size: 180%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
 
+</style>
+"""
 
-def main():
-    st.markdown(
-        """
-        <style>
-        .reportview-container {
-            background: url("scr/fondo.jpg") no-repeat center center fixed;
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True)
-    st.title('Dashboard de Power BI en Streamlit')
-    
-    # Inserta el iframe con la URL de tu dashboard de Power BI
-    st.markdown('<iframe width="1000" height="800" align = "center" src="https://app.powerbi.com/view?r=eyJrIjoiM2NjNDA0YmItMmRhZC00ZDhlLWFmOWYtZTZiMWMxYWY3ODAzIiwidCI6ImUyYjc5Nzc5LTBhODgtNDMzMS05YjQyLTM4NGNkNzFjODVkNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
-        
-if __name__ == '__main__':
-    main()
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("It's summer!")
+st.sidebar.header("Configuration")
